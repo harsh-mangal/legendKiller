b5a3a50a1bd015f178c434a0ff8e7e23d3911b78
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2025-09-10 03:02:30 +0530

    added new code and fixed

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is client 
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    this is admin
+    </div>
+  );
+}
+
+export default App;
